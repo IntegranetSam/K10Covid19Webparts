78d95af68c6c49f1cadc9f4c198e65625e57059d
vasu <vasu.yerramsetta@raybiztech.com>
2020-05-01 00:38:55 +0530

    Added Global folder and files into App_Code Updated .gitignore accordingly

---
+++ CMS/App_Code/Global/COVID-19/CountryInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for CountryInfo
+/// </summary>
+public class CountryInfo
+{
+    public string Location { get; set; }
+    public string Logo { get; set; }
+    public int Recovered { get; set; }
+    public int Deaths { get; set; }
+    public int Confirmed { get; set; }
+    public DateTime LastChecked { get; set; }
+    public DateTime LastReported { get; set; }
+}
---
+++ CMS/App_Code/Global/COVID-19/JSONResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for JSONResponse
+/// </summary>
+public class JSONResponse
+{
+    public bool Error { get; set; }
+    public long StatusCode { get; set; }
+    public string Message { get; set; }
+    public CountryInfo Data { get; set; }
+}
